a6e6f5bf171fa3c09fb4c623c23368719404ba12
Orlando Javier <oj.flores118@gmail.com>
2023-11-30 11:14:58 -0600

    diseño navbar

--- components/Dashboard/navbar.vue
+++ components/Dashboard/navbar.vue
@@ -2,9 +2,15 @@
       <v-app-bar color="#ffffff">
         <v-toolbar-title>Vue Vuetify Navbar</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon>
+        <v-btn icon color="black">
             <v-icon>mdi mdi-bell-badge</v-icon>
         </v-btn>
-        <v-btn> Available </v-btn>
+        <v-btn rounded="sm" color="#4FB783" elevation="4" density="compact">
+          <v-avatar size="32" class="mr-2">
+            <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+          </v-avatar>
+          <span style="color: white;">Available</span>
+        </v-btn>
       </v-app-bar>
 </template>
+
