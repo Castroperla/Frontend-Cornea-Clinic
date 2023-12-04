069d54df74a6326cdbad55409e1f557acb0940a9
Naydelin Zavala <150888285+Nayde666@users.noreply.github.com>
2023-12-03 20:01:23 -0600

    Add files via upload

---
+++ pages/toDoList/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <to-DoList />
+  </div>
+</template>
+
+<script>
+  import toDoList from '~/components/Users/toDoList.vue'
+
+  export default {
+    layout: 'dashboard',
+    components:{
+      toDoList
+    }
+  }
+</script>
+
